e47431b7f2496e8149a947a257f60dcf283ef070
marciolimasl <marciolimasl2002@gmail.com>
2023-02-13 12:30:27 -0300

    Responsiveness

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,10 +1,10 @@
 import "./styles.css";
 
-const Hero = ({ imageSrc }) => {
+const Hero = ({ imageSrc, title }) => {
   return (
     <section className="hero">
       <img src={imageSrc} alt="Travel" className="hero_image" />
-      <h1 className="hero_title">Travel made simple.</h1>
+      <h1 className="hero_title">{title}</h1>
     </section>
   );
 };
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./styles.css";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = ({ navbarLinks }) => {
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  const toggleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
+
   return (
     <nav className="navbar">
       <span className="navbar_logo">Travel</span>
+
+      {menuClicked ? (
+        <FiX size={25} className="navbar__menu" onClick={toggleMenuClick} />
+      ) : (
+        <FiMenu size={25} className="navbar__menu" onClick={toggleMenuClick} />
+      )}
+
       <ul className="navbar_list">
         {navbarLinks.map((item) => {
           return (
